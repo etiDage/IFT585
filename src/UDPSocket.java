e6c7a024265d03e3683218305f96ec5b70114900
Xavier Lemieux <xavier7.lemieux@hotmail.com>
2021-06-03 11:36:20 -0400

    removed random comment

--- src/UDPSocket.java
+++ src/UDPSocket.java
@@ -26,30 +26,12 @@ public class UDPSocket
                 File result = new File(args[1]);
                 Receiver receiver = new Receiver(port, IPAddress, packetSize, ackPort);
                 receiver.ReceiveFile(result);
-                /*byte[] receiveData = new byte[1024];
-                DatagramSocket serverSocket = new DatagramSocket(50500);
-                System.out.println("ReceiveMode");
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-                String modifiedSentence = new String(receivePacket.getData());
-                System.out.println("FROM SERVER: " + modifiedSentence);
-                serverSocket.close();
-                break;*/
                 break;
             case "T":
                 File input = new File(args[1]);
 
                 Transmitter transmitter = new Transmitter(packetSize, IPAddress, ackPort, input);
                 transmitter.transmitFile(port);
-                /*DatagramSocket clientSocket = new DatagramSocket();
-                byte[] sendData = new byte[1024];
-                System.out.println("TransmitMode");
-                String sentence = "Hello World";
-                sendData = sentence.getBytes();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 50500);
-                clientSocket.send(sendPacket);
-                clientSocket.close();
-                break;*/
                 break;
         }
     }
